7dae8be20bec23ef8c48e5158e2ea6a3b5078ccb
Palash Dhavle <palashdhavle15@gmail.com>
2024-06-07 16:31:03 +0530

    feat: create model for tasks and custom user

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Staus(models.IntegerChoices):
+    TODO = 1,'Todo'
+    IN_PROGRESS = 2,'In_porgress'
+    DONE = 3,'Done'
+
+# custom user model for storing user auth and other data
+class CustomUser(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=50)
+
+    # this is the field which will be used when loggin in
+    USERNAME_FIELD = "email"
+    # required field when creating superuser, email and password is required by default
+    REQUIRED_FIELDS = ['first_name','last_name','username',]
+
+class Tasks(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey('api.CustomUser' ,related_name='tasks', on_delete=models.CASCADE)
+    task = models.CharField(max_length=50)
+    status = models.IntegerField(choices=Staus.choices, default=1)
+    description = models.TextField(max_length=200)
+    due = models.DateTimeField()
+
+    class Meta:
+        verbose_name = ("Task")
+        verbose_name_plural = ("Tasks")
+
+    def __str__(self):
+        return self.name
