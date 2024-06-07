3022d37e37ec332e6e9cb452574121ab5166217a
Palash Dhavle <palashdhavle15@gmail.com>
2024-06-07 22:50:52 +0530

    feat: update URLs config to support new views

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,10 +3,11 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("signup/",views.UserCreate.as_view()),
     path('', include('rest_framework.urls')), # enables sessions auth for browsable API
+    path("signup/",views.UserCreate.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("tasks/",views.TaskListCreate.as_view())
+    path("tasks/",views.TaskListCreate.as_view(), name='task_create_list'),
+    path("tasks/<str:pk>",views.TaskRetriveUpdateDelete.as_view(), name='task_create_list')
 ]
 
